0ceeeb47b0906ceac79ec03c46a768ce722b17e5
League Student <league@iMac.attlocal.net>
2019-07-03 16:33:11 -0700

    module 2

--- src/_04_validation/Validation.java
+++ src/_04_validation/Validation.java
@@ -11,15 +11,35 @@ public class Validation {
 	public static void main(String[] args) {
 		
 		Random randomMaker = new Random();
-		
-		int randomNumber = randomMaker.nextInt(5);
-		
-		System.out.println(randomNumber);
+	
+	
 
 		// 1. Use each value of randomNumber to give the user a random compliment.
 
 		// 2. Repeat all the code above 10 times
+for (int i = 0; i <= 10; i++) {
+	int randomNumber = randomMaker.nextInt(5);
+	if(randomNumber == 0) {
+		System.out.println("you are cool");
+	} 
+	if(randomNumber == 1) { 
+		System.out.println("you are pretty");
+	} 
+	if(randomNumber == 2) { 
+		System.out.println("you are awesome");  
+	} 
+	if(randomNumber == 3) { 
+		System.out.println("you are kind"); 
+	} 
+	if(randomNumber == 4) { 
+		System.out.println("you are fun");
+	}
+}
+		// 3. Find someone to test out your program. They will like it :) 
 		
-		// 3. Find someone to test out your program. They will like it :)
+		
+		
+		
+		
 	}
 }
